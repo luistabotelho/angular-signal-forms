7019aeaa8d1d4f12b00e15a9a8d65f78ba774309
luistabotelho <66958065+luistabotelho@users.noreply.github.com>
2024-11-20 23:43:17 +0100

    Reorganized exports

--- projects/signal-forms/src/lib/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './base-type.interface'
-export * from './signal-forms-definition.interface'
-export * from './signal-forms-options.interface'
-export * from './signal-forms.interface'
