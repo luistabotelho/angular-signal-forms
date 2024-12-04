b552d1d4d1a5d4be735db2c2d9dde0237d438211
luistabotelho <luistabotelho@gmail.com>
2024-12-04 10:39:00 +0100

    Updated example component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { signalForm, signalFormValue, signalFormValid, resetSignalForm, signalFormSetTouched } from '../../projects/signal-forms/src/public-api';
+import { Component, computed } from '@angular/core';
+import { signalForm, signalFormValue, signalFormValid, resetSignalForm, signalFormSetTouched, signalFormGroup } from '../../projects/signal-forms/src/public-api';
 import { FormsModule } from '@angular/forms';
 import { signalFormErrors } from '../../projects/signal-forms/src/lib/helpers/signal-form-errors.helper';
 import { CommonModule } from '@angular/common';
@@ -9,6 +9,11 @@ interface DataType {
   field1Child: string
   field2: string
   dateField: string
+}
+
+interface TableItem {
+  id: string
+  name: string
 }
 
 @Component({
@@ -49,6 +54,32 @@ export class AppComponent {
   $formValue = signalFormValue(this.form)
   $formErrors = signalFormErrors(this.form)
   $formValid = signalFormValid(this.form)
+
+  $tableData = signalFormGroup<TableItem>({
+    id: {
+      initialValue: '',
+      validators: [
+        val => !val ? new Error('Required') : null
+      ]
+    },
+    name: {
+      initialValue: '',
+      validators: undefined
+    }
+  })
+
+  $tableValid = this.$tableData.valid()
+  $tableErrors = this.$tableData.errors()
+  $tableValue = this.$tableData.value()
+
+  $completeValid = computed(() => this.$formValid() && this.$tableValid())
+  $completeErrors = computed(() => [...this.$formErrors(), ...this.$tableErrors()])
+  $completeValue = computed(() => ({
+    ...this.$formValue(),
+    table: [
+      ...this.$tableValue()
+    ]
+  }))
 
   resetForm = () => resetSignalForm(this.form)
   
