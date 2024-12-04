e4fc93599c9db1982846afcc44ba48b11fa5e0e4
luistabotelho <luistabotelho@gmail.com>
2024-12-04 13:11:13 +0100

    Added tests

--- projects/signal-forms/src/lib/signal-forms.module.spec.ts
+++ projects/signal-forms/src/lib/signal-forms.module.spec.ts
@@ -9,6 +9,8 @@ import { signalFormValid } from "./helpers/signal-form-valid.helper";
 import { signalFormValue } from "./helpers/signal-form-value.helper"
 import { signalFormOptionsDefaults } from "./config/defaults.config"
 import { signalFormSetTouched } from "./helpers/signal-form-set-touched.helper"
+import { signalFormGroup } from "./signal-form-group.module"
+import { SignalFormGroup } from "./interfaces/signal-form-group.interface"
 
 interface ITestObject {
     field1: string
@@ -17,37 +19,38 @@ interface ITestObject {
     field3: Date
 }
 
-describe('signalForms', () => {
-    let testObject: SignalFormDefinition<ITestObject> = {
-        field1: {
-            initialValue: "Placeholder"
-        },
-        field1Child: {
-            initialValue: "",
-            validators: [
-                (val, form) => !val && form.field1.currentValue() ? new Error("Required if field1 given") : null
-            ]
-        },
-        field2: {
-            initialValue: 0,
-            validators: [
-                val => !val ? new Error("Required") : null
-            ]
-        },
-        field3: {
-            initialValue: new Date('2024-11-20T09:30:11'),
-            validators: [
-                val => val.toISOString().slice(0,10) < '2024-11-21' ? new Error("Must be after 2024-11-21") : null,
-                val => val.getHours() != 0 ? new Error("Should be midnight") : null
-            ]
-        }
+let signalFormDefinition: SignalFormDefinition<ITestObject> = {
+    field1: {
+        initialValue: "Placeholder"
+    },
+    field1Child: {
+        initialValue: "",
+        validators: [
+            (val, form) => !val && form.field1.currentValue() ? new Error("Required if field1 given") : null
+        ]
+    },
+    field2: {
+        initialValue: 0,
+        validators: [
+            val => !val ? new Error("Required") : null
+        ]
+    },
+    field3: {
+        initialValue: new Date('2024-11-20T09:30:11'),
+        validators: [
+            val => val.toISOString().slice(0,10) < '2024-11-21' ? new Error("Must be after 2024-11-21") : null,
+            val => val.getHours() != 0 ? new Error("Should be midnight") : null
+        ]
     }
+}
+
+describe('signalForms', () => {
     let form: SignalForm<ITestObject>
     let customForm: SignalForm<ITestObject>
 
     beforeEach(() => {
-        form = signalForm(testObject)
-        customForm = signalForm(testObject, {
+        form = signalForm(signalFormDefinition)
+        customForm = signalForm(signalFormDefinition, {
             requireTouched: false,
             errorState: 'myError',
             defaultState: 'myDefault'
@@ -150,7 +153,7 @@ describe('signalForms', () => {
     describe('signalFormValue', () => {
         it('should return an object of T in SignalForm<T>', () => {
             let $value = signalFormValue(form)
-            let originalObjectKeys = Object.keys(testObject).sort()
+            let originalObjectKeys = Object.keys(signalFormDefinition).sort()
             let keys = Object.keys($value()).sort()
             expect(keys).toEqual(originalObjectKeys)
             expect($value().field1).toBe('Placeholder')
@@ -163,6 +166,67 @@ describe('signalForms', () => {
             Object.entries(form).forEach(([key, field]) => {
                 expect(field.touched()).withContext(key).toBeTrue()
             })
+        })
+    })
+})
+
+describe('signalFormGroup', () => {
+    let formGroup: SignalFormGroup<ITestObject>
+
+    beforeEach(() => {
+        formGroup = signalFormGroup(signalFormDefinition)
+        formGroup.addItem()
+    })
+
+    describe('.addItem()', () => {
+        it('should allow adding an item', () => {
+            formGroup.addItem()
+            expect(formGroup.data().length).withContext('signalFormGroup.length').toBe(2)
+        })
+    })
+
+    describe('.removeItem()', () => {
+        it('should allow removing an item by index', () => {
+            formGroup.addItem()
+            formGroup.data()[1].field1.currentValue.set("Updated")
+            formGroup.removeItem(0)
+            expect(formGroup.data().length).withContext('signalFormGroup.length').toBe(1)
+            expect(formGroup.data()[0].field1.currentValue()).withContext('last form in group value').toBe("Updated")
+        })
+    })
+
+    describe('.valid()', () => {
+        it('should return a signal validating all forms', () => {
+            let form0 = formGroup.data()[0]
+            let today = new Date()
+            today.setHours(0,0,0,0)
+            form0.field1.currentValue.set("Valid")
+            form0.field1Child.currentValue.set("Also Valid")
+            form0.field2.currentValue.set(1)
+            form0.field3.currentValue.set(today)
+            expect(formGroup.valid()()).withContext('form group with single valid item').toBeTrue()
+            formGroup.addItem()
+            expect(formGroup.valid()()).withContext('form group after adding invalid item').toBeFalse()
+        })
+    })
+
+    describe('.errors()', () => {
+        it('should return a signal containing an array of errors', () => {
+            let $errors = formGroup.errors()
+            formGroup.addItem()
+            expect($errors()).toBeInstanceOf(Array)
+            expect($errors().length).toBe(8)
+            let field1ChildError = 'field1Child: Required if field1 given'
+            expect($errors()[0]).toBe(field1ChildError)
+            expect($errors()[4]).toBe(field1ChildError)
+        })
+    })
+
+    describe('.value()', () => {
+        it('should return a signal containing an array of T', () => {
+            let formValue = formGroup.value()()
+            expect(formValue).toBeInstanceOf(Array)
+            expect(formValue[0].field1).toBe('Placeholder')
         })
     })
 })
