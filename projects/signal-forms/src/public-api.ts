f8c80a5fddf1413756031c4faedb62aa2a5e5dec
luistabotelho <66958065+luistabotelho@users.noreply.github.com>
2025-06-20 19:47:58 +0200

    Exported missing signalFormErrors api (#23)
    * Exposed missing api
    
    * Updated demo component

--- projects/signal-forms/src/public-api.ts
+++ projects/signal-forms/src/public-api.ts
@@ -9,6 +9,7 @@ export {signalFormValid} from './lib/helpers/signal-form-valid.helper'
 export {resetSignalForm} from './lib/helpers/reset-signal-form.helper'
 export {signalFormValue} from './lib/helpers/signal-form-value.helper'
 export {signalFormSetTouched} from './lib/helpers/signal-form-set-touched.helper'
+export {signalFormErrors} from './lib/helpers/signal-form-errors.helper'
 
 export {SignalForm} from './lib/interfaces/signal-forms.interface'
 export {SignalFormOptions} from './lib/interfaces/signal-forms-options.interface'
